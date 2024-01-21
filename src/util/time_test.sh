0b70a9ea9c26b04f77413fc5dfcd22dc483ccded
Ivan <73799907+IvanGrigorik@users.noreply.github.com>
2024-01-22 00:30:13 +0300

    Add some time-getting functions (#9)

--- src/util/time_test.sh
+++ src/util/time_test.sh
@@ -18,24 +18,59 @@ function test_now_millis() {
         local ms
         ms=$(time_now_millis) || \
                 assert_fail
-        assert_ge "${ms}" 1689708545316
+        
+        [ "${ms}" -le 1689708545316 ] && assert_fail
+        return 0
 }
 readonly -f test_now_millis
 
-function test_time_now_day() {
+function test_time_now_day_of_week() {
         local day
-        day=$(time_now_day) || \
+        day=$(time_now_day_of_week) || \
                 assert_fail
-        assert_gt "${day}" 0
-        assert_lt "${day}" 8
+
+        [ "${day}" -lt 0 ] && assert_fail
+        [ "${day}" -gt 8 ] && assert_fail
+        return 0
 }
-readonly -f test_time_now_day
+readonly -f test_time_now_day_of_week
+
+function test_time_now_day_of_week_str() {
+        local day
+        day=$(time_now_day_of_week_str) || \
+                assert_fail
+
+        case "${day}" in
+        "Monday"|"monday") ;;
+        "Tuesday"|"tuesday") ;;
+        "Wednesday"|"wednesday") ;;
+        "Thursday"|"thursday") ;;
+        "Friday"|"friday") ;;
+        "Saturday"|"saturday") ;;
+        "Sunday"|"sunday") ;;
+        *) assert_fail "non-existing day of the week";;
+        esac
+}
+readonly -f test_time_now_day_of_week_str
+
+function test_time_now_day_of_month() {
+        local day
+        day=$(time_now_day_of_month) || \
+                assert_fail
+
+        [ "${day}" -lt 0 ] && assert_fail
+        [ "${day}" -gt 32 ] && assert_fail
+        return 0
+}
+readonly -f test_time_now_day_of_month
 
 function test_time_now_year() {
         local year
         year=$(time_now_year) || \
                 assert_fail
-        assert_eq "$($X_DATE +%Y)" "${year}"
+        
+        [ "$($X_DATE +%Y)" != "${year}" ] && assert_fail "$($X_DATE +%Y) not equal to ${year}"
+        return 0
 }
 readonly -f test_time_now_year
 
@@ -43,9 +78,34 @@ function test_time_now_month() {
         local month
         month=$(time_now_month) || \
                 assert_fail
-        assert_eq "$($X_DATE +%m)" "${month}"
+
+        [ "$($X_DATE +%m)" != "${month}" ] && assert_fail "$($X_DATE +%Y) not equal to ${year}"
+        return 0
 }
 readonly -f test_time_now_month
+
+function test_time_now_month_str() {
+        local month
+        month=$(time_now_month_str) || \
+                assert_fail
+                
+        case "${month}" in
+        "Jan"|"January") ;;
+        "Feb"|"February") ;;
+        "Mar"|"March") ;;
+        "Apr"|"April") ;;
+        "May") ;;
+        "Jun"|"June") ;;
+        "Jul"|"July") ;;
+        "Aug"|"August") ;;
+        "Sep"|"September") ;;
+        "Oct"|"October") ;;
+        "Nov"|"November") ;;
+        "Dec"|"December") ;;
+        *) assert_fail "non-existent month";;
+        esac
+}
+readonly -f test_time_now_month_str
 
 function test_time_duration() {
         function _f() {
@@ -54,7 +114,9 @@ function test_time_duration() {
 
         local ec=0
         time_duration_w "ABC" _f > /dev/null || ec=$?
-        assert_eq 22 ${ec}
+        
+        [ ${ec} -ne 22 ] && assert_fail
+        return 0
 }
 readonly -f test_time_duration
 
@@ -62,7 +124,9 @@ function test_time_millis_to_seconds() {
         local secs
         secs=$(time_millis_to_seconds 2000) || \
                 assert_fail
-        assert_eq 2 "${secs}"
+        
+        [ ${secs} -ne 2 ] && assert_fail
+        return 0
 }
 readonly -f test_time_millis_to_seconds
 
@@ -70,12 +134,14 @@ function test_time_millis_to_date() {
         local actual
         actual=$(time_millis_to_date "1670357961396") || \
                 assert_fail
-        assert_eq "2022-12-06" "${actual% *}"
+
+        [ "2022-12-06" != "${actual% *}" ] && assert_fail
+        return 0
 }
 readonly -f test_time_millis_to_date
 
 function test_time_num_to_month() {
-        assert_eq "Mar" $(time_num_to_month 3)
+        [ "Mar" != "$(time_num_to_month 3)" ] && assert_fail
 
         local ctx
 
@@ -84,11 +150,12 @@ function test_time_num_to_month() {
                 assert_fail
         ctx_show $ctx | grep 'incorrect month number' || \
                 assert_fail
+        return 0
 }
 readonly -f test_time_num_to_month
 
 function test_time_month_to_num() {
-        assert_eq 3 $(time_month_to_num "Mar")
+        [ 3 -ne  $(time_month_to_num "Mar") ] && assert_fail
 
         local ctx
 
